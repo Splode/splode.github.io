69e2f14cbcd25577c3eccc37ce168da7d2736c6f
Christopher Murphy <christopherianmurphy@gmail.com>
2017-05-27 10:38:41 -0600

    Testing Piranha
    Testing mobile nav.

--- piranha-test/js/main.js
+++ piranha-test/js/main.js
@@ -2,11 +2,11 @@
 $(document).ready(function() {
 
 	//smooth scroll
-	$('a').click(function(){
-		$('html, body').animate({
-			scrollTop: $( $(this).attr('href') ).offset().top
-		}, 750);
-		return false;
-	});
-  
+	// $('a').click(function(){
+	// 	$('html, body').animate({
+	// 		scrollTop: $( $(this).attr('href') ).offset().top
+	// 	}, 750);
+	// 	return false;
+	// });
+
 });
